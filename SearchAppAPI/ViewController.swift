6b34da85cd6d208589082e4ef3f9d7d38cb1c4ab
soyung sim <simsy1024@gmail.com>
2024-08-10 09:19:09 +0900

    Initial Commit

---
+++ SearchAppAPI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SearchAppAPI
+//
+//  Created by 심소영 on 8/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
